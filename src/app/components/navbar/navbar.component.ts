ba31c223984ccaab8d95806accda0b50290a069d
Kevin Chen <ningchen227@gmail.com>
2019-07-18 23:08:05 -0500

    Navbar fix

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { 
+  @ViewChild('navBurger') navBurger: ElementRef;
+  @ViewChild('navMenu') navMenu: ElementRef;
+
+  toggleNavbar() {
+    this.navBurger.nativeElement.classList.toggle("is-active");
+    this.navMenu.nativeElement.classList.toggle("is-active");
   }
+
+  constructor() { }
 
   ngOnInit() {
   }
-
 }
