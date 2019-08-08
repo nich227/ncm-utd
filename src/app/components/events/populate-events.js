7c01e7bbf30924cbc9532ba2ba9dda985073d247
Kevin Chen <ningchen227@gmail.com>
2019-08-07 22:41:34 -0500

    Implementing checks if event is already passed

--- src/app/components/events/populate-events.js
+++ src/app/components/events/populate-events.js
@@ -2,24 +2,27 @@ function populateEvents() {
   //Get JSON file from repo
   $.getJSON("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", function (json) {
     //Get today's date
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
 
     today = yyyy + '-' + mm + '-' + dd;
 
     var numEvent = 1;
-    $.each(json.events, function(numEvent, event) {
-      $("#eventTmpl").clone().attr("id", "event" + numEvent).appendTo(".section").css("display", "block");
-      //Modify all elements within the new event
-      $("#event" + numEvent + " .event-pic").attr("src", event.pic_location);
-      $("#event" + numEvent + " .event-title").html(event.name);
-      $("#event" + numEvent + " .event-desc").html(event.description);
-      $("#event" + numEvent + " .event-date").html("<i class='fas fa-calendar-alt'></i> " + event.date_string);
-      $("#event" + numEvent + " .event-loc").html("<i class='fas fa-map-marker-alt'></i> " + event.location);
-      $("#event" + numEvent + " .event-loc-link").attr("href", event.maps_link);
-      numEvent++;
+    $.each(json.events, function (numEvent, event) {
+      let end_date = new Date(events.end_date);
+      if (end_date > today) {
+        $("#eventTmpl").clone().attr("id", "event" + numEvent).appendTo(".section").css("display", "block");
+        //Modify all elements within the new event
+        $("#event" + numEvent + " .event-pic").attr("src", event.pic_location);
+        $("#event" + numEvent + " .event-title").text(event.name);
+        $("#event" + numEvent + " .event-desc").text(event.description);
+        $("#event" + numEvent + " .event-date").html("<i class='fas fa-calendar-alt'></i> " + event.date_string);
+        $("#event" + numEvent + " .event-loc").html("<i class='fas fa-map-marker-alt'></i> " + event.location);
+        $("#event" + numEvent + " .event-loc").attr("href", event.maps_link);
+        numEvent++;
+      }
     });
   });
 
