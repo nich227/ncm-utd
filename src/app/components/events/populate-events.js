5be97adcaf2a10457a69d5d6ca386d031443792e
Kevin Chen <ningchen227@gmail.com>
2019-08-07 21:41:26 -0500

    Moved location of json

--- src/app/components/events/populate-events.js
+++ src/app/components/events/populate-events.js
@@ -1,13 +1,28 @@
 function parseEvents() {
-  $.getJSON("events.json", function(json) {
-    alert(json);
+  //Get JSON file from repo
+  $.getJSON("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", function (json) {
+    alert(json.events[0].location);
   });
 
-  putEvents();
+  //putEvents();
 }
 
 function putEvents() {
+  //Get today's date
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = yyyy + '-' + mm + '-' + dd;
+
   let numEvent = 0;
-  numEvent++;
-  $("#eventTmpl").clone().attr("id", "event" + numEvent).appendTo(".section").css("display", "block");
+  json.forEach(function (event) {
+    $("#eventTmpl").clone().attr("id", "event" + numEvent).appendTo(".section").css("display", "block");
+    //Modify all elements within the new event
+    $("#event"+numEvent+" .event-title").css("value", "yeet");
+
+    numEvent++;
+  });
+
 }
