d998fd8b533329bea6484a6f76f12b83f93a48a1
Kevin Chen <ningchen227@gmail.com>
2019-08-07 23:14:35 -0500

    Making event location optional

--- src/app/components/events/populate-events.js
+++ src/app/components/events/populate-events.js
@@ -8,22 +8,26 @@ function populateEvents() {
     //Iterate through JSON file for events
     var numEvent = 1;
     $.each(json.events, function (numEvent, event) {
+      //NOTE: end_date is in UNIX time, *1000 for milliseconds
       let end_date = new Date(event.end_date * 1000);
 
-      //Check if the event has already passed
+      //Check if the event date has already passed
       if (today < end_date) {
         $("#eventTmpl").clone().attr("id", "event" + numEvent).appendTo(".section").css("display", "block");
         //Modify all elements within the new event
         $("#event" + numEvent + " .event-pic").attr("src", event.pic_location);
-        if(typeof event.pic_scale !== 'undefined'){
-            $("#event" + numEvent + " .event-pic").attr("width", event.pic_scale);
-            $("#event" + numEvent + " .event-pic").attr("height", event.pic_scale);
+        if (typeof event.pic_scale !== 'undefined') {
+          $("#event" + numEvent + " .event-pic").attr("width", event.pic_scale);
+          $("#event" + numEvent + " .event-pic").attr("height", event.pic_scale);
         }
         $("#event" + numEvent + " .event-title").text(event.name);
         $("#event" + numEvent + " .event-desc").text(event.description);
         $("#event" + numEvent + " .event-date").html("<i class='fas fa-calendar-alt'></i> " + event.date_string);
         $("#event" + numEvent + " .event-loc").html("<i class='fas fa-map-marker-alt'></i> " + event.location);
-        $("#event" + numEvent + " .event-loc").attr("href", event.maps_link);
+        if (typeof event.maps_link !== 'undefined')
+          $("#event" + numEvent + " .event-loc").attr("href", event.maps_link);
+        else
+          $("#event" + numEvent + " .event-loc").attr("href", "");
         numEvent++;
       }
     });
